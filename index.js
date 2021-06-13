22569192dea5ec5461d3433d1757bc83cdb776f7
Kovalenko Makcim <max.kow@yandex.ru>
2021-06-13 10:48:09 +0300

    ДЗ 11
    Выполнен обратный таймер согласно ДЗ

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ const refs = {
 	timerDays: document.querySelector('[data-value="days"]'),
 	timerHours: document.querySelector('[data-value="hours"]'),
 	timerMins: document.querySelector('[data-value="mins"]'),
-	timerSecs: document.querySelector('[data-value="secs"]')
+	timerSecs: document.querySelector('[data-value="secs"]'),
+	mainTitle: document.querySelector('.main-title')
 }
-
 
 class Timer {
 	constructor ({onTick}) {
+		this.targetDate = 'Jul 17 2021';
+		// this.targetDate = 'Jun 13 2021 10:38:00';
+
+		this.timeIntervalId = null;
 		this.onTick = onTick;
-		
 	}
 	pad(value) {
 		return String(value).padStart(2, '0')
 	}
 
 	onTimeComponents(time) {
+		if (Date.parse(time) <= Date.parse(new Date())) {
+			const total = 0;
+			const days = 0;
+			const hours = 0;
+			const mins = 0;
+			const secs = 0;
+			return {total, days, hours, mins, secs };
+		}
 		const total = Date.parse(time) - Date.parse(new Date());
 		const days = this.pad(Math.floor(total / (1000 * 60 * 60 * 24)));
 		const hours = this.pad(Math.floor((total % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
@@ -25,33 +36,32 @@ class Timer {
 		return {total, days, hours, mins, secs }
 	}
 	
-	initializeClock(time) {
-		
-		function updateClockFace() {
-			const t = timer.onTimeComponents(time);
-
-			refs.timerDays.innerHTML = t.days;
-			refs.timerHours.innerHTML = ('0' + t.hours).slice(-2);
-			refs.timerMins.innerHTML = ('0' + t.mins).slice(-2);
-			refs.timerSecs.innerHTML = ('0' + t.secs).slice(-2);
-
-			if (t.total <= 0) {
-				clearInterval(timeinterval);
-			}
-		}		
-		updateClockFace()
-		const timeinterval = setInterval(updateClockFace, 1000)
+	initializeClock() {
+		this.onTick();
+		this.timeIntervalId = setInterval(updateClockFace, 1000)
 	}
 }
 
 const timer = new Timer({
-	// onTick: updateClockFace,
+	onTick: updateClockFace,
 });
 
-// const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-const deadline='2021-06-14';
+function updateClockFace() {
+	const t = timer.onTimeComponents(timer.targetDate);
 
-timer.initializeClock(deadline)
+	refs.timerDays.innerHTML = t.days;
+	refs.timerHours.innerHTML = ('0' + t.hours).slice(-2);
+	refs.timerMins.innerHTML = ('0' + t.mins).slice(-2);
+	refs.timerSecs.innerHTML = ('0' + t.secs).slice(-2);
+
+	if (t.total <= 0) {
+		clearInterval(timer.timeIntervalId);
+		refs.mainTitle.textContent = 'Распродажа закончилась!!!';
+		refs.mainTitle.style.color = '#ff0000';
+	}
+}
+
+timer.initializeClock()
 
 
 
