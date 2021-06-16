afa215884d4711e32204dcf65fe29f1f53e69868
Kovalenko Makcim <max.kow@yandex.ru>
2021-06-16 21:05:39 +0300

    дз 11
    исправлены ошибки

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const refs = {
-	// timerDays: document.querySelector('[data-value="days"]'),
-	// timerHours: document.querySelector('[data-value="hours"]'),
-	// timerMins: document.querySelector('[data-value="mins"]'),
-	// timerSecs: document.querySelector('[data-value="secs"]'),
 	mainTitle: document.querySelector('.main-title')
 }
-
 
 class Timer {
 	constructor ({onTick, targetDate, selector }) {
@@ -13,9 +8,30 @@ class Timer {
 		this.selector = selector;
 
 		this.timeIntervalId = null;
-		// this.onTick = onTick;	
-	}	
+		this.onTick = onTick;	
+	}		
 	
+	initializeClock() {		
+		const clock = document.querySelector(this.selector);
+		const timerDays = clock.querySelector('[data-value="days"]');
+		const timerHours = clock.querySelector('[data-value="hours"]');
+		const timerMins = clock.querySelector('[data-value="mins"]');
+		const timerSecs = clock.querySelector('[data-value="secs"]');
+
+		this.timeIntervalId = setInterval(() => {
+			if (Number(timerDays.textContent) === 0 & Number(timerHours.textContent) === 0 & Number(timerMins.textContent) === 0 & Number(timerSecs.textContent) === 0) {
+				clearInterval(this.intervalId);
+				refs.mainTitle.textContent = 'Распродажа закончилась!!!';
+				refs.mainTitle.style.color = '#ff0000';
+				return
+			}
+
+			const times = this.onTimeComponents(this.targetDate);
+			this.onTick(times, timerDays, timerHours, timerMins, timerSecs);
+
+		}, 1000)			
+	}
+
 	pad(value) {
 		return String(value).padStart(2, '0')
 	}
@@ -36,54 +52,21 @@ class Timer {
 		const secs = this.pad(Math.floor((total % (1000 * 60)) / 1000));
 
 		return {total, days, hours, mins, secs }
-	}
-	
-	initializeClock() {
-		const clock = document.querySelector(this.selector);
-		const timerDays = clock.querySelector('[data-value="days"]');
-		const timerHours = clock.querySelector('[data-value="hours"]');
-		const timerMins = clock.querySelector('[data-value="mins"]');
-		const timerSecs = clock.querySelector('[data-value="secs"]');
-		
-		const targets = this.targetDate;
-		const intervalId = this.timeIntervalId;
-		
-		
-		if (Number(timerDays.textContent) === 0 & Number(timerHours.textContent) === 0 & Number(timerMins.textContent) === 0 & Number(timerSecs.textContent) === 0) {
-			clearInterval(intervalId);
-			return
-		}
-		
-		
-		function updateClockFace() {
-			const t = timer.onTimeComponents(targets);
-			
-			if (Number(timerDays.textContent) === 0 & Number(timerHours.textContent) === 0 & Number(timerMins.textContent) === 0 & Number(timerSecs.textContent) === 0) {
-				clearInterval(intervalId);
-				refs.mainTitle.textContent = 'Распродажа закончилась!!!';
-				refs.mainTitle.style.color = '#ff0000';
-				return;
-			};
+	}	
+}
 
-			timerDays.innerText = t.days;
-			timerHours.innerHTML = ('0' + t.hours).slice(-2);
-			timerMins.innerHTML = ('0' + t.mins).slice(-2);
-			timerSecs.innerHTML = ('0' + t.secs).slice(-2);
-		}
-
-		updateClockFace()
-		// this.onTick();
-		this.timeIntervalId = setInterval(updateClockFace, 1000)		
-	}
-
-	
+function updateClockFace({days, hours, mins, secs}, timerDays, timerHours, timerMins, timerSecs) {
+	timerDays.innerHTML = days;
+	timerHours.innerHTML = ('0' + hours).slice(-2);
+	timerMins.innerHTML = ('0' + mins).slice(-2);
+	timerSecs.innerHTML = ('0' + secs).slice(-2);
 }
 
 
 const timer = new Timer({
-	targetDate : 'Jun 16 2021 19:39:00',
+	targetDate : 'Jun 22 2021 21:00:00',
 	selector : '#timer-1',
-	// onTick: updateClockFace,
+	onTick: updateClockFace,
 });
 
 timer.initializeClock()
